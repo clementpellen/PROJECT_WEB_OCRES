f1a46a8603962ac671e9d65746881f859005d868
Quentin Dstl <qdesautel@gmail.com>
2021-11-30 12:31:17 +0100

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,16 @@ import ChildLandingPage from './landingpages/ChildLandingPage.js';
 import ParentLandingPage from './landingpages/ParentLandingPage';
 import SchoolLandingPage from './landingpages/SchoolLandingPage.js';
 
-function App() {
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+
+export default function App() {
   return (
-    // <ChildLandingPage />
-    // <ParentLandingPage />
-    <SchoolLandingPage />
+    <Router>
+      <Routes>
+        <Route path="/lpenfant" caseSensitive={false} element={<ChildLandingPage />} />
+        <Route path="/lpparent" caseSensitive={false} element={<ParentLandingPage />} />
+        <Route path="/lpecole" caseSensitive={false} element={<SchoolLandingPage />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
