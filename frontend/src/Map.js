7e1ae5b6f925cfb5e2440a5581c03a9c45600ff2
Quentin Dstl <qdesautel@gmail.com>
2021-12-01 16:03:59 +0100

    ajout de point et ajout css

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker} from 'google-maps-react';
 
 const mapStyles = {
     width: '100%',
@@ -7,21 +7,62 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+    
+    state = {
+        showingInfoWindow: false,  // Hides or shows the InfoWindow
+        activeMarker: {},          // Shows the active marker upon click
+        selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+    };
+
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+    });
+
+    onClose = props => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            });
+        }
+    };
+    
     render() {
         return (
             <Map
                 google={this.props.google}
                 zoom={13}
                 style={mapStyles}
-                initialCenter={
-                    {
-                        lat: 48.8534,
-                        lng: 2.3488
-                    }
-                }
-            />
+                initialCenter={{lat: 48.8534, lng: 2.3488}}
+            >
+                <Marker
+                    position={{lat: 48.851346, lng: 2.291883}}
+                    onClick={this.onMarkerClick}
+                    name={"Balard la zone"}
+                />
+                <Marker
+                    position={{lat: 48.831390, lng: 2.271667}}
+                    onClick={this.onMarkerClick}
+                    name={"L'Arene"}
+                />
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}
+                    onClose={this.onClose}
+                >
+                    <div>
+                        <h4>{this.state.selectedPlace.name}</h4>
+                    </div>
+                </InfoWindow>
+            </Map>
         );
     }
+
+    
+        
 }
 
 export default GoogleApiWrapper({
