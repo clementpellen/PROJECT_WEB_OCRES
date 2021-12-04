ff9f604ad430e87f9531c8a0a8447f1f6f85c0bc
Quentin Dstl <qdesautel@gmail.com>
2021-12-04 13:04:34 +0100

    move maps dans widgets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,25 @@ import ChildLandingPage from './landingpages/ChildLandingPage.js';
 import ParentLandingPage from './landingpages/ParentLandingPage.js';
 import SchoolLandingPage from './landingpages/SchoolLandingPage.js';
 import FieldReservationWidget from './Widgets/FieldReservationWidget.js';
-import Map from './Map.js';
+import Map from './Widgets/Map.js';
 
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Map />} />
-        <Route path="/lpenfant" caseSensitive={false} element={<ChildLandingPage />} />
-        <Route path="/lpparent" caseSensitive={false} element={<ParentLandingPage />} />
-        <Route path="/lpecole" caseSensitive={false} element={<SchoolLandingPage />} />
-        <Route path="/api" caseSensitive={false} element={
-          <FieldReservationWidget position={{ lat: 48.831390, lng: 2.271667 }} day="4" time="16:00"/>
-        } />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Map />} />
+				<Route path="/lpenfant" caseSensitive={false} element={<ChildLandingPage />} />
+				<Route path="/lpparent" caseSensitive={false} element={<ParentLandingPage />} />
+				<Route path="/lpecole" caseSensitive={false} element={<SchoolLandingPage />} />
+				<Route path="/api" caseSensitive={false} element={
+					<div>
+						{console.log(new Date().getUTCHours() + 2)}
+						<FieldReservationWidget position={{ lat: 48.831390, lng: 2.271667 }} day="4" time="16:00"/>
+					</div>
+				} />
+			</Routes>
+		</Router>
+	);
 }
