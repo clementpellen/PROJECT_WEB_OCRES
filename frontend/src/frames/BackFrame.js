2734712ce7255d820dbf7f0fa886a1b17760a1e7
clementpellen <clementpellen@gmail.com>
2021-12-05 00:45:07 +0100

    avancée du responsaive landing page école

--- frontend/src/frames/BackFrame.js
+++ frontend/src/frames/BackFrame.js
@@ -4,18 +4,43 @@ import '../reset.css';
 import './BackFrame.css';
 
 class BackFrame extends React.Component {
-
+    
     constructor(props) {
         super(props);
+        this.state = {animation_margin_top : 0}
+    }
+
+    componentWillMount() {
+        this.determineAnimationMarginTop();
     }
     
+    determineAnimationMarginTop() {
+        switch (this.props.backframe_id) {
+            case "school-backframe":
+                this.setState({
+                    animation_margin_top : window.innerHeight - 700
+                });
+                break;
+        
+            default:
+                break;
+        }
+    }
 
     render() {
+        
+        var style;
+
+        if(this.state.animation_margin_top > 0)
+            style = this.state.animation_margin_top + 'rem';
+        else 
+            style = 0 + 'rem';
+
         return (
             <main id={this.props.backframe_id} className='BackFrame'>
                 <h1 className='Corner'>Corner</h1>
                 <h4 className='value-proposition'>{this.props.value_proposition}</h4>
-                <img id={this.props.animation_id} className='animation' src={this.props.animation} alt="animation" />
+                <img id={this.props.animation_id} className='animation' src={this.props.animation} alt="animation" style={{marginTop : style}}/>
             </main>
         );
     }
--- frontend/src/landingpages/SchoolLandingPage.js
+++ frontend/src/landingpages/SchoolLandingPage.js
@@ -4,6 +4,7 @@ import ScrollingFrame from '../frames/ScrollingFrame.js';
 import '../Parallax.css';
 
 import school_animation from '../img/gif/school_animation.gif';
+import React from 'react';
 
 /* Page des différents segments clients */
 const SCHOOL_BACKFRAME = "school-backframe";
@@ -23,29 +24,47 @@ const SCHOOL_TEASING_PROPOSITION = "Qu'est-ce que Corner peut apporter à mon ly
 /* Description du Widget des différents segments clients */
 const SCHOOL_DESCRIPTION_WIDGET = "lycées nous font déjà confiance";
 
-function SchoolLandingPage() {
-    return(
-        <div className='SchoolLandingPage'>
-            
-            <BackFrame className='BackFrame' 
-                backframe_id={SCHOOL_BACKFRAME} 
-                value_proposition={SCHOOL_VALUE_PROPOSITION} 
-                animation_id={SCHOOL_ANIMATION_ID}
-                animation={school_animation} /> 
-            
-            <div className='FrontFrame'>  
-                <div className='transparent-block'/> 
+class SchoolLandingPage extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {transparent_block_height : 0}
+    }
+
+    componentWillMount() {
+        this.setState({
+            transparent_block_height : window.innerHeight - 185
+        });
+    }
+    
+    render() {
+
+        var style = this.state.transparent_block_height + 'rem';
+
+        return(
+            <div className='SchoolLandingPage'>
                 
-                <ScrollingFrame
-                    cta={SCHOOL_CTA} 
-                    teaser_proposition={SCHOOL_TEASING_PROPOSITION} 
-                    widget_type={"schools"} 
-                    description_widget={SCHOOL_DESCRIPTION_WIDGET}/>
+                <BackFrame className='BackFrame' 
+                    backframe_id={SCHOOL_BACKFRAME} 
+                    value_proposition={SCHOOL_VALUE_PROPOSITION} 
+                    animation_id={SCHOOL_ANIMATION_ID}
+                    animation={school_animation} /> 
+                
+                <div className='FrontFrame'>  
+                    <div className='transparent-block' style={{height : style}}/> 
+                    
+                    <ScrollingFrame
+                        cta={SCHOOL_CTA} 
+                        teaser_proposition={SCHOOL_TEASING_PROPOSITION} 
+                        widget_type={"schools"} 
+                        description_widget={SCHOOL_DESCRIPTION_WIDGET}/>
+
+                </div>
 
             </div>
+        );
+    }
 
-        </div>
-    );
 }
 
 export default SchoolLandingPage;
