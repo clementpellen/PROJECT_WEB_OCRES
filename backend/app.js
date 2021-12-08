1edc8ffa89c46aaa065dccde5088eaf288af1d97
Quentin Dstl <qdesautel@gmail.com>
2021-12-08 08:41:53 +0100

    delete some stuff

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var followersRouter = require("./routes/followers");
 var reservationsRouter = require("./routes/reservations");
 
@@ -16,8 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/followers", followersRouter);
 app.use("/reservations", reservationsRouter);
 
